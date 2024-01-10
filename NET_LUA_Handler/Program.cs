7f9472efc481f5b4867b659e60f2444d64dde4b9
Leonid Gavrilov <g.leonid@dogovor24.kz>
2024-01-10 23:16:15 +0600

    Добавьте файлы проекта.

---
+++ NET_LUA_Handler/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_LUA_Handler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string scriptname = Console.ReadLine();
+            string executablePath = Assembly.GetEntryAssembly()?.Location;
+            string currentDirectory = Path.GetDirectoryName(executablePath);
+            string luaInterpreter = currentDirectory + "\\Lua\\lua53.exe"; 
+
+            string luaScriptPath = currentDirectory + "\\scripts\\" + scriptname + ".lua"; 
+
+          
+            Process process = new Process();
+
+          
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = luaInterpreter, 
+                Arguments = luaScriptPath, 
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true 
+            };
+
+            process.StartInfo = startInfo;
+
+
+            process.Start();
+
+
+            string output = process.StandardOutput.ReadToEnd();
+            Console.WriteLine("Output:");
+            Console.WriteLine(output);
+
+            process.WaitForExit();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+           
+
+       
+        }
+    }
+}
